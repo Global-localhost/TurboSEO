ed1e54cb0af3cfe10861e137f1ff3efc6ef61383
Phonemetra Inc <devteam@phonemetra.com>
2019-03-19 19:48:16 +0100

    Initial commit

---
+++ wp-seo.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Turbo SEO Plugin.
+ *
+ * @package   WPSEO\Main
+ * @copyright Copyright (C) 2019, Phonemetra - support@phonemetra.com
+ * @license   http://www.apache.org/licenses/ Apache License 2.0
+ *
+ * @wordpress-plugin
+ * Plugin Name: Turbo SEO
+ * Description: 
+ * Author:      Dev Team Phonemetra
+ * Author URI:  
+ * Text Domain: 
+ * Domain Path: 
+ * License:     
+ *
+ */
